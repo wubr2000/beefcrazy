adf38f95e71bd1d240c1b16afcef75689e6559d2
Bruno Wu <wubr2000@bruno-wus-macbook-pro.local>
2013-12-24 19:13:07 +0800

    find_by only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   helper_method :current_user
   
   def current_user
-    @current_user ||= User.find_or_create_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
   
   def is_authenticated?
